91abefb49da71037ac9128bd19fbcfdb5cf5236a
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-02-14 09:01:43 +0800

    Show App on window

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,31 +1,25 @@
-import androidx.compose.material.MaterialTheme
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
-
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
-}
+import androidx.compose.ui.window.rememberWindowState
+import com.jianastrero.hsle.App
+import com.jianastrero.hsle.Constants
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    val windowState = rememberWindowState(
+        width = Constants.WINDOW_WIDTH,
+        height = Constants.WINDOW_HEIGHT,
+        position = WindowPosition(Alignment.Center)
+    )
+    Window(
+        state = windowState,
+        title = "HL Save Editor",
+        resizable = false,
+        undecorated = true,
+        transparent = true,
+        onCloseRequest = ::exitApplication
+    ) {
+        App(::exitApplication)
     }
 }
