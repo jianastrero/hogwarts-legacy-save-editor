bf49fd776bafc7e4d222e5565e6f5d810eae960c
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-02-14 11:30:19 +0800

    Create typography theme

---
+++ src/jvmMain/kotlin/com/jianastrero/hsle/typography/Typography.kt
@@ -0,0 +1,15 @@
+package com.jianastrero.hsle.typography
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
+
+val NotoSerifFontFamily = FontFamily(
+    Font("fonts/NotoSerifKhojki-Regular.ttf"),
+    Font("fonts/NotoSerifKhojki-Bold.ttf", FontWeight.Bold),
+    Font("fonts/NotoSerifKhojki-SemiBold.ttf", FontWeight.SemiBold),
+    Font("fonts/NotoSerifKhojki-Medium.ttf", FontWeight.Medium),
+)
+
+val NotoSerifTypography = Typography(defaultFontFamily = NotoSerifFontFamily)
